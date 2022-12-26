7ec3324f4083b9a00283d9fff4838fc48c009bf1
Julien VIGNAU-ESPINE <julien@cognitive-design-systems.com>
2022-12-26 23:44:04 +0100

    :feat: changing interface

--- window.py
+++ window.py
@@ -19,17 +19,34 @@ from youtube_functions import *
 # =============================================================================
 
 def clicked():
+    """Button Placeholder"""
     print(txt.get())
 
 def download():
+    """Search and dowload the video on MP3 format"""
     download_from_search(txt.get())
 
+# Window configuration
 window = Tk()
-window.title("Youtube To MP3")
-window.geometry('500x300')
-txt = Entry(window,width=10)
-txt.grid(column=0, row=0)
-btn = Button(window, text="Click Me", command=download)
-btn.grid(column=2, row=0)
+window.title("YouTube To MP3")
+window.geometry('800x500')
+window.configure(bg="#333")
 
+# Title configuration
+title = Label(window, text="YouTube to MP3", font=("Arial Bold", 50), bg="#333",
+fg="#F00")
+title.grid(column=0, row=0)
+
+# Text input configuration
+title = Label(window, text="Music title to download", font=("Arial Bold", 15),
+bg="#333", fg="#FFF")
+title.grid(column=0, row=5)
+txt = Entry(window,width=40)
+txt.grid(column=0, row=6)
+
+# Button configuration
+btn = Button(window, text="Download", command=download)
+btn.grid(column=0, row=10)
+
+# Run the window
 window.mainloop()
