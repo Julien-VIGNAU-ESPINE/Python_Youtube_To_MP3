9150e7a1f40aeb6a4eecfe5ac2d843bfc0b6f399
Julien VIGNAU-ESPINE <julien@cognitive-design-systems.com>
2022-12-26 22:58:59 +0100

    :feat: adding function to dowload mp3 from youtube url

---
+++ youtube_functions.py
@@ -0,0 +1,28 @@
+# YOUTUBE_TO_MP3
+# YouTube Functions
+# Author : Julien VIGNAU-ESPINE
+# 26/12/2022 22:55
+
+# =============================================================================
+# IMPORTATIONS
+# =============================================================================
+
+import youtube_dl
+
+# =============================================================================
+# CODE
+# =============================================================================
+
+def download_audio(yt_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([yt_url])
+
+download_audio("https://www.youtube.com/watch?v=o6RQuIbzwJk")
